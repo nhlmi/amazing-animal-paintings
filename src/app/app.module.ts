9fdf899a742131d1a1f00b2cef489de4cbd88d88
Nuran Hilmi <nuran.hilmi.karim@pwc.com>
2023-08-24 16:58:55 +0800

    section 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     ProductModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-cart-view',
+  templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.css']
+})
+export class CartViewComponent implements OnInit {
+
+  cartItems: Product[] = []
+  totalPrice: number = 0
+
+  constructor(
+    private cartService: CartService
+  ){}
+  
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(x =>{
+      this.cartItems = x
+      this.totalPrice = this.getTotalPrice()
+    })
+  }
+
+  getTotalPrice(): number{
+    let total = 0
+    for(let item of this.cartItems){
+      total += item.price
+    }
+
+    return total;
+  }
+
+}
